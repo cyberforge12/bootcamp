a9f1766f821c000e17f934c9e9724061a1720c73
mmanhack <mmanhack@school21>
2020-04-16 19:27:40 +0300

    fixed day02/ex03

--- day01/ex03/generator.py
+++ day01/ex03/generator.py
@@ -1,23 +1,31 @@
 # function prototype
-import struct
 import time
+import numpy as np
 
 def randbelow(n):
     return (7 * time.time() + 11) % n
 
 def generator(text, sep=" ", option=None):
     """Option is an optional arg, sep is mandatory"""
-    txt = text.split(sep)
-    if option == "shuffle":
-        for i in reversed(range(1, len(txt))):
-        # pick an element in text[:i+1] with which to exchange x[i]
-            j = int(randbelow(i + 1))
-            txt[i], txt[j] = txt[j], txt[i]
-    elif option == "ordered":
-        return sorted(txt, key=str.casefold)
-    return txt
+    if type(text) == str:
+        txt = text.split(sep)
+        if option == "shuffle":
+            for i in reversed(range(1, len(txt))):
+            # pick an element in text[:i+1] with which to exchange x[i]
+                j = int(randbelow(i + 1))
+                txt[i], txt[j] = txt[j], txt[i]
+        elif option == "ordered":
+            return sorted(txt, key=str.casefold)
+        elif option == "unique":
+            return np.unique(np.array(txt))
+        elif option not in [None, 'unique', 'ordered', 'shuffle']:
+            return ["ERROR"]
+        return txt
+    else:
+        return ["ERROR"]
 
-text = "Le Lorem Ipsum est simplement du faux texte."
+text = "Le Lorem Ipsum est simplement du faux texte. Repeat Repeat Repeat"
+print("\nno option:")
 for word in generator(text, sep=" "):
     print(word)
 
@@ -27,4 +35,17 @@ for word in generator(text, sep=" ", option="shuffle"):
 
 print("\nordered:")
 for word in generator(text, sep=" ", option="ordered"):
+    print(word)
+
+print("\nunique:")
+for word in generator(text, sep=" ", option="unique"):
+    print(word)
+print("\nsep=\".\":")
+for word in generator(text, sep="."):
+    print(word)
+print("\nERROR option:")
+for word in generator(text, sep=" ", option="ERROR"):
+    print(word)
+print("\nERROR text:")
+for word in generator(12312312, sep=" ", option="unique"):
     print(word)
