93f3aa05908f789a34e72b6431c5696759e61e4f
mmanhack <mmanhack@school21>
2020-04-16 20:15:52 +0300

    carve out test.py from generator.py in day02/ex03

--- day01/ex03/generator.py
+++ day01/ex03/generator.py
@@ -1,4 +1,3 @@
-# function prototype
 import time
 import numpy as np
 
@@ -23,29 +22,3 @@ def generator(text, sep=" ", option=None):
         return txt
     else:
         return ["ERROR"]
-
-text = "Le Lorem Ipsum est simplement du faux texte. Repeat Repeat Repeat"
-print("\nno option:")
-for word in generator(text, sep=" "):
-    print(word)
-
-print("\nshuffle:")
-for word in generator(text, sep=" ", option="shuffle"):
-    print(word)
-
-print("\nordered:")
-for word in generator(text, sep=" ", option="ordered"):
-    print(word)
-
-print("\nunique:")
-for word in generator(text, sep=" ", option="unique"):
-    print(word)
-print("\nsep=\".\":")
-for word in generator(text, sep="."):
-    print(word)
-print("\nERROR option:")
-for word in generator(text, sep=" ", option="ERROR"):
-    print(word)
-print("\nERROR text:")
-for word in generator(12312312, sep=" ", option="unique"):
-    print(word)
